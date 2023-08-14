a4fe76c11aa463e3b6dbf8769d6128e3c58dc361
buyekeobare <buyekeobare@gmail.com>
2023-08-14 00:13:05 -0700

    Updated

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,7 +1,5 @@
--- inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server
--- New row:
---     id = 89
---     name = Best school
+-- lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
+-- All fields should be printed
 -- The database name will be passed as an argument of the mysql command
 
-INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Best School');
+SELECT * FROM first_table;
