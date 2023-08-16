0bab16b36ab9408c26961f8e86b7eb54954050bc
buyekeobare <buyekeobare@gmail.com>
2023-08-17 00:12:01 +0300

    Updated file

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Write a script that creates the MySQL server user
 
 CREATE USER IF DOES NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
