de131b9554c5dcd10e0729bca05e88baa0b0aca0
buyekeobare <buyekeobare@gmail.com>
2023-08-16 23:16:19 +0300

    Updated file

--- SQL_more_queries/0-privileges.sql
+++
@@ -1,9 +0,0 @@
---- Write a script that lists all privileges of the MySQL users 
-
-<<<<<<< HEAD
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
-=======
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
->>>>>>> 3f635993114e0f4f9b4ffea161a99a76a1c362f7
