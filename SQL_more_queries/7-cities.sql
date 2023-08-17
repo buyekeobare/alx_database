c8abcb46e9b4553b875b9e7c9588fd8a826c3148
buyekeobare <buyekeobare@gmail.com>
2023-08-17 22:45:21 +0300

    Upadted file

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,4 +1,7 @@
 -- Write a script that creates the database hbtn_0d_usa and the table cities
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, state_id INT NOT NULL FOREIGN KEY, name VARCHAR(256) NOT NULL);
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+  state_id INT NOT NULL, name VARCHAR(256) NOT NULL, 
+  FOREIGN KEY (state_id) REFERENCES htbn_0d_usa.states(id));
